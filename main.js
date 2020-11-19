c150ba27d4e89fa257e2f357526befb4af978d1a
Stanislav <verman19837@gmail.com>
2020-11-19 20:11:39 +0700

    create base functional of game

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-const firstRow = 'мама мыла раму'; 
-const secondRow = 'собака друг человека';
+const $btn = document.getElementById('btn-kick');
+const character = {
+	name: 'Pikachu',
+	defaultHP: 100,
+	damageHP: 100,
+	elHp: document.getElementById('health-character'),
+	elProgressbar: document.getElementById('progressbar-character'),
+}
 
-function getRow(firstRow, secondRow) {
-	let char1, char2;
+const enemy = {
+	name: 'Charmander',
+	defaultHP: 100,
+	damageHP: 100,
+	elHp: document.getElementById('health-enemy'),
+	elProgressbar: document.getElementById('progressbar-enemy'),
+}
 
-	for (let i = 0; i < firstRow.length; i++) {
-		if (firstRow[i] === 'a') {
-			char1++;
-		}
-	}
-	for(let i = 0; i < secondRow.length; i++) {
-		if (secondRow[i] === 'a') {
-			char2++;
-		}
-	}
-	if (char1 > char2) {
-		return firstRow;
-	} else {
-		return secondRow;
-	}
+$btn.addEventListener('click', function() {
+	console.log('Kick');
+	changeHP(random(20), character);
+	changeHP(random(20), enemy);
+});
+
+function init() {
+	console.log('Start Game!');
+	renderHP(character);
+	renderHP(enemy);
+}
+
+function renderHP(person) {
+	renderHPLife(person);
+	renderProgressbarHP(person);
 }
 
 
+function renderHPLife(person) {
+	person.elHp.innerText = person.damageHP + ' / '+ person.defaultHP;
+	
+}
 
-console.log(getRow(firstRow, secondRow));
+function renderProgressbarHP(person) {
+	person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP(count, person) {
+	if (person.damageHP < count) {
+		person.damageHP = 0;
+		alert(person.name + 'Проиграл');
+		$btn.disabled = true;
+	}else{
+		person.damageHP -= count;
+	}
+
+	renderHP(person);
+}
+
+function random(num) {
+	return Math.ceil(Math.random() * num);
+}
+
+init();
